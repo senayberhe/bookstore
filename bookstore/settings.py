438783d386507fb46be818222bf7e61789d8f495
senay berhe <tsionberhe@gmail.com>
2023-04-05 14:05:51 -0700

    created pages and testing

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     
     # LOCAL 
     "accounts.apps.AccountsConfig",
+    "pages.apps.PagesConfig",
 ]
 
 MIDDLEWARE = [
@@ -51,7 +52,7 @@ ROOT_URLCONF = "bookstore.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
